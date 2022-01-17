7b868894647de46a6f4a9b4189cd34159a84f27a
yannick.s3 <yannick.s3@schule.bremen.de>
2022-01-17 11:20:13 +0100

    Objekte

--- Obst/src/Obst.java
+++ Obst/src/Obst.java
@@ -1,44 +1,43 @@
 public abstract class Obst {
 
     private String name;
-   private double gewicht;
+    private double gewicht;
     private String farbe;
     private boolean reife;
 
     //Konstruktor
-    public Obst(String name){
+    public Obst(String name) {
         this.name = name;
     }
 
     //Getter
-    public String getName(){
+    public String getName() {
         return name;
     }
 
-    public String getFarbe(){
+    public String getFarbe() {
         return farbe;
     }
 
-    public void setFarbe(String farbe){
+    public void setFarbe(String farbe) {
         this.farbe = farbe;
     }
 
-    public double getGewicht(){
+    public double getGewicht() {
         return gewicht;
     }
 
-    public void setGewicht(double gewicht){
-        this.gewicht=gewicht;
+    public void setGewicht(double gewicht) {
+        this.gewicht = gewicht;
     }
 
-    public boolean isReife(){
+    public boolean isReife() {
         return reife;
     }
 
     public void setReife(boolean reife) {
         this.reife = reife;
     }
-
 
 
 }
