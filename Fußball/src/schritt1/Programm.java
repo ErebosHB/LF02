3900369921aea5ff938073209f98c4b744fe212e
yannick.s3 <yannick.s3@schule.bremen.de>
2022-01-20 13:00:41 +0100

    Fußball

--- Fußball/src/schritt1/Programm.java
+++ Fußball/src/schritt1/Programm.java
@@ -1,5 +1,7 @@
 package schritt1;
 
+import schritt2.Torwart;
+
 public class Programm {
     public static void main(String[] args) {
         Trainer trainer = new Trainer("Hans",50,9);
@@ -8,10 +10,14 @@ public class Programm {
 
         Spieler spieler = new Spieler("Messi",34,6,10,7,678);
 
+        Torwart torwart = new Torwart("Ter Stegen",29,7,7,9);
+
 
 
         System.out.println(trainer);
         System.out.println();
         System.out.println(spieler);
+        System.out.println();
+        System.out.println(torwart);
     }
 }
---
+++ Fußball/src/schritt2/Torwart.java
@@ -0,0 +1,26 @@
+package schritt2;
+
+import schritt1.Spieler;
+
+public class Torwart extends Spieler {
+
+    private int reaktion;
+
+    public Torwart(String name, int alter, int staerke,int motivation,int reaktion){
+        super(name,alter,staerke,0,motivation,0);
+        this.reaktion = reaktion;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString()+ "\n\tReaktion\t= " + reaktion;
+    }
+
+    public int getReaktion() {
+        return reaktion;
+    }
+
+    public void setReaktion(int reaktion) {
+        this.reaktion = reaktion;
+    }
+}
