91c75d8e9f52fc5407103b03f1d5be34c19b9626
yannick.s3 <yannick.s3@schule.bremen.de>
2022-02-16 11:08:45 +0100

    Konto fertig

--- Konto/src/Kunde.java
+++ Konto/src/Kunde.java
@@ -1,7 +1,15 @@
 public class Kunde extends Person {
     private Mitarbeiter mitarbeiter;
-    public Kunde(String vorname,String nachname, Mitarbeiter mitarbeiter){
+    public Kunde(String vorname,String nachname, Mitarbeiter mitarbeiter,Adresse adresse){
         super(vorname,nachname);
+        this.mitarbeiter = mitarbeiter;
+    }
+
+    public Mitarbeiter getMitarbeiter() {
+        return mitarbeiter;
+    }
+
+    public void setMitarbeiter(Mitarbeiter mitarbeiter) {
         this.mitarbeiter = mitarbeiter;
     }
 
--- Konto/src/Programm.java
+++ Konto/src/Programm.java
@@ -1,22 +1,62 @@
 public class Programm {
+
     public static void main(String[] args) {
-        Adresse adresse = new Adresse("lehrer-lämpel-weg","bremen",28329,2);
-        Mitarbeiter mitarbeiter = new Mitarbeiter("Phil","Jones");
+        Adresse adresseMitarbeiter = new Adresse("Schlossallee", "2", "04711", "Entenhausen");
+        Adresse adresseKunde = new Adresse("ParkstraÃŸe", "36a", "12345", "Buxtehude");
 
-        Kunde kunde = new Kunde("lucas","jones", mitarbeiter);
-        kunde.setAdresse(adresse);
-        Girokonto girokonto = new Girokonto(2, 10, kunde);
-        girokonto.setKontostand(2000);
-        Girokonto girokonto1 = new Girokonto(2, 2000, kunde);
-        System.out.println(kunde);
+        Mitarbeiter mitarbeiter = new Mitarbeiter("Holger", "Geldfuchs");
 
-        girokonto.ueberweisen(girokonto1, 200);
-        System.out.println("neuer kotostand: "+girokonto1.getKontostand());
-        if (girokonto.auszahlen(2000)){
-            System.out.println(girokonto.getKontostand());
-        }else{
-            System.out.println("zu wenig geld zum auszahlen :(");
+        Kunde kunde1 = new Kunde("Klaus", "Sparbier", mitarbeiter, adresseKunde);
+        Kunde kunde2 = new Kunde("Volker", "Vielgeld", mitarbeiter, adresseKunde);
+        kunde1.setMitarbeiter(mitarbeiter);
+
+        Girokonto girokonto1 = new Girokonto(15.0,10000,kunde1);
+        Girokonto girokonto2 = new Girokonto(15.0,10000,kunde2);
+        Sparkonto sparkonto1 = new Sparkonto(10,kunde1);
+
+        System.out.println(mitarbeiter);
+        System.out.println(kunde1);
+        System.out.println(kunde2);
+        System.out.println(girokonto1);
+        System.out.println(girokonto2);
+        System.out.println(sparkonto1);
+        System.out.println("--------------------");
+
+        if (girokonto1.einzahlen(2000.0)) {
+            System.out.println(girokonto1);
+        } else {
+            System.out.println("Fehler beim Einzahlen: Einzahlbetrag ist kleiner 0");
+        }
+
+        if (girokonto1.auszahlen(20.0)) {
+            System.out.println(girokonto1);
+        } else {
+            System.out.println("Fehler beim Auszahlen: Guthaben und Kreditlimit kleiner als Auszahlbetrag");
+        }
+
+        if (girokonto1.ueberweisen(girokonto2,10.0)) {
+            System.out.println(girokonto2);
+            System.out.println(girokonto1);
+        } else {
+            System.out.println("Fehler beim Ãœberweisen");
+        }
+
+        System.out.println("--------------------");
+        System.out.println("---- Ãœberweisung ---");
+        if (girokonto2.auszahlen(200.0)) {
+            System.out.println(girokonto2);
+        } else {
+            System.out.println("Fehler beim Auszahlen: Guthaben und Kreditlimit kleiner als Auszahlbetrag");
+        }
+        System.out.println("--------------------");
+
+        if (girokonto1.ueberweisen(girokonto2,1.0)) {
+            System.out.println(girokonto1);
+        } else {
+            System.out.println("Fehler beim Ãœberweisen");
         }
 
     }
+
 }
+
