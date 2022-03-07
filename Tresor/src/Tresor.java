27256ca0318a6d5c62f976dbbf10b713b6f6bfe2
yannick.s3 <yannick.s3@schule.bremen.de>
2022-03-07 11:09:23 +0100

    Tresor

--- Tresor/src/Tresor.java
+++ Tresor/src/Tresor.java
@@ -1,7 +1,11 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 public class Tresor {
     private ArrayList<Gegenstand> gegenstandsliste;
+    private double gesamtwert;
 
     public Tresor(ArrayList<Gegenstand> gegenstandsliste){
         this.gegenstandsliste = gegenstandsliste;
@@ -16,10 +20,29 @@ public class Tresor {
     }
 
     public double gesamtwert(ArrayList<Gegenstand> gegenstandsliste){
+        for (Gegenstand i : gegenstandsliste
+             ) {
+
+            gesamtwert += i.getWert();
+        }
+        return gesamtwert;
+    }
+
+    public Gegenstand ermittelGegenstand(ArrayList<Gegenstand> gegenstandsliste) throws IOException {
+        System.out.println("Bitte geben Sie die ID ein, die Sie suchen.");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String eingabe = br.readLine();
+        int objekt = 0;
+        for (Gegenstand i : gegenstandsliste
+        ) {
+            if (eingabe == gegenstandsliste.get(objekt).getId()) {
+                System.out.println("Dieser Gegenstand ist im Tresor");
+                return gegenstandsliste.get(objekt);
+            }
+            objekt++;
+        }
+        if (eingabe != gegenstandsliste.get().getId()){
 
     }
 
-    public String ermittelGegenstand(ArrayList<Gegenstand> gegenstandsliste){
-
-    }
 }
